25d2ad88bd1d9cb430110139ad76a748f9b1fd01
Thomas Schnittcher <thomas.schnittcher@t-online.de>
2017-02-10 12:59:40 +0100

    Signed-off-by: Thomas Schnittcher <thomas.schnittcher@t-online.de>

--- TS_HBGarage/module.php
+++ TS_HBGarage/module.php
@@ -159,6 +159,28 @@ TargetDoorState
         $JSON['Buffer'] = utf8_encode('{"topic": "callback", "Characteristic": "'.$Characteristic.'", "Device": "'.$DeviceName.'", "value": "'.$result.'"}');
         $Data = json_encode($JSON);
         $this->SendDataToParent($Data);
+
+        $Characteristic ="ObstructionDetected";
+        $JSON['DataID'] = "{018EF6B5-AB94-40C6-AA53-46943E824ACF}";
+        $JSON['Buffer'] = utf8_encode('{"topic": "setValue", "Characteristic": "'.$Characteristic.'", "Device": "'.$DeviceName.'", "value": "0"}');
+        $Data = json_encode($JSON);
+        $this->SendDataToParent($Data);
+
+        if ($result == "1" ) {
+          $Characteristic = "CurrentDoorState";
+          $JSON['DataID'] = "{018EF6B5-AB94-40C6-AA53-46943E824ACF}";
+          $JSON['Buffer'] = utf8_encode('{"topic": "setValue", "Characteristic": "'.$Characteristic.'", "Device": "'.$DeviceName.'","value": "1"}');
+          $Data = json_encode($JSON);
+          $this->SendDataToParent($Data);
+        }
+        if ($result == "0" ) {
+          $Characteristic = "CurrentDoorState";
+          $JSON['DataID'] = "{018EF6B5-AB94-40C6-AA53-46943E824ACF}";
+          $JSON['Buffer'] = utf8_encode('{"topic": "setValue", "Characteristic": "'.$Characteristic.'", "Device": "'.$DeviceName.'","value": "0"}');
+          $Data = json_encode($JSON);
+          $this->SendDataToParent($Data);
+        }
+
         return;
       }
     }
