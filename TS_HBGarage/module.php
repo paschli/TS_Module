9167808790be2cbb4df45cc6e3d803c136e4778b
Thomas Schnittcher <thomas.schnittcher@t-online.de>
2017-02-10 11:12:46 +0100

    Signed-off-by: Thomas Schnittcher <thomas.schnittcher@t-online.de>

--- TS_HBGarage/module.php
+++ TS_HBGarage/module.php
@@ -137,6 +137,20 @@ TargetDoorState
         $JSON['Buffer'] = utf8_encode('{"topic": "callback", "Characteristic": "'.$Characteristic.'", "Device": "'.$DeviceName.'", "value": "'.$result.'"}');
         $Data = json_encode($JSON);
         $this->SendDataToParent($Data);
+      
+        if ($result == 1 ) {
+          $JSON['DataID'] = "{018EF6B5-AB94-40C6-AA53-46943E824ACF}";
+          $JSON['Buffer'] = utf8_encode('{"topic": "callback", "Characteristic": "CurrentPosition", "Device": "'.$DeviceName.'", "value": "100"}');
+          $Data = json_encode($JSON);
+          $this->SendDataToParent($Data);
+        }
+        if ($result == 0 ) {
+          $JSON['DataID'] = "{018EF6B5-AB94-40C6-AA53-46943E824ACF}";
+          $JSON['Buffer'] = utf8_encode('{"topic": "callback", "Characteristic": "CurrentPosition", "Device": "'.$DeviceName.'", "value": "0"}');
+          $Data = json_encode($JSON);
+          $this->SendDataToParent($Data);
+        }
+        
         return;
       }
     }
